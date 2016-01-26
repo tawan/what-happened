a96ed39e6be78dc46c576ce9fcd6f5d31e3d6696
Tawan Sierek <tawan.sierek@gmail.com>
2016-01-26 22:20:54 +0100

    Require paper_trail only when Rails is loaded

--- lib/what_happened.rb
+++ lib/what_happened.rb
@@ -3,5 +3,5 @@ require 'what_happened/dsl_support/dsl_support'
 require 'what_happened/config'
 require 'what_happened/event'
 require 'what_happened/notification'
-require 'paper_trail'
+require 'paper_trail' if defined? Rails
 require 'what_happened/railtie' if defined? Rails
