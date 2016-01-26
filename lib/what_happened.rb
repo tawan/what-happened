961f3699e16a3d0db12b8a664e9cb3860f566010
Tawan Sierek <tawan.sierek@gmail.com>
2016-01-26 17:19:09 +0100

    Add railtie

--- lib/what_happened.rb
+++ lib/what_happened.rb
@@ -3,5 +3,5 @@ require 'what_happened/dsl_support/dsl_support'
 require 'what_happened/config'
 require 'what_happened/event'
 require 'what_happened/notification'
-
-module WhatHappened; end;
+require 'paper_trail'
+require 'what_happened/railtie' if defined? Rails
---
+++ lib/what_happened/railtie.rb
@@ -0,0 +1,17 @@
+module WhatHappened
+  class Railtie < Rails::Railtie
+    initializer "what_happened.create_config" do |app|
+      app.config.x.what_happened = Config.new
+
+      app.config.after_initialize do
+        path = File.join(app.root, "config", "what_happened.rb")
+        if File.exist?(path)
+          app.config.x.what_happened.instance_eval(File.read(path))
+          PaperTrail::Version.after_create do |version|
+            app.config.x.what_happened.broadcast(version)
+          end
+        end
+      end
+    end
+  end
+end
