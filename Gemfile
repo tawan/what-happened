8eab0265efa2fb4e6a511e397fdc98d436c5cd15
Tawan Sierek <tawan.sierek@gmail.com>
2016-01-26 18:04:20 +0100

    Add travis config
    Trigger travis build
    
    Remove byebug from bundle
    It destroys the 1.9.x build [1].
    
    [1] https://travis-ci.org/tawan/what-happened/jobs/104943119

--- Gemfile
+++ Gemfile
@@ -3,6 +3,5 @@ source 'https://rubygems.org'
 gemspec
 gem 'rspec', '~> 3.4'
 gem 'rspec-rails'
-gem 'byebug'
 gem 'sqlite3'
 gem 'factory_girl'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require 'bundler/setup'
 Bundler.setup
 
 require 'what_happened'
-require 'byebug'
 
 RSpec.configure do |config|
   config.filter_run_excluding slow: true if ENV['SPEC_ALL'] != 'true'
