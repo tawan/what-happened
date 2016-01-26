0c8c1f325d09510f6e18ab33c59d86f2407f1b7d
Tawan Sierek <tawan.sierek@gmail.com>
2016-01-26 14:13:43 +0100

    Add byebug

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'bundler/setup'
 Bundler.setup
 
 require 'what_happened'
+require 'byebug'
 
 RSpec.configure do |config|
   config.filter_run_excluding slow: true if ENV['SPEC_ALL'] != 'true'
