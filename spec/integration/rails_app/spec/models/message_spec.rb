86d8f1cf3697d39e3cbe1df74ee33acde5b031ff
Tawan Sierek <tawan.sierek@gmail.com>
2016-01-26 17:57:15 +0100

    Add integration test

--- spec/integration/rails_app/spec/models/message_spec.rb
+++ spec/integration/rails_app/spec/models/message_spec.rb
@@ -1,5 +1,27 @@
 require File.expand_path('../../rails_helper', __FILE__)
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Message.paper_trail_enabled_for_model? }
+  it { is_expected.to be true }
+
+  describe "create" do
+    let(:sender) { create(:user, name: "Marvin") }
+    let(:recipient) { create(:user, name: "John") }
+
+    it "creates a version" do
+      expect(PaperTrail::Version.all).to be_empty
+      m = create(:message, sender: sender, recipient: recipient)
+      expect(PaperTrail::Version.all.length).to be 1
+      version = PaperTrail::Version.first
+      expect(version.item.id).to eq(m.id)
+    end
+
+    it "creates a notification" do
+      expect(WhatHappened::Notification.all).to be_empty
+      m = create(:message, sender: sender, recipient: recipient)
+      expect(WhatHappened::Notification.all.length).to be 1
+      notification = WhatHappened::Notification.first
+      expect(notification.recipient.id).to eq(recipient.id)
+    end
+  end
 end
