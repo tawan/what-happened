97a8e0a49bdb48bc584f28653e571ea4dc3e9760
Tawan Sierek <tawan.sierek@gmail.com>
2016-01-28 18:22:17 +0100

    WIP

--- spec/integration/rails_app/config/what_happened.rb
+++ spec/integration/rails_app/config/what_happened.rb
@@ -1,6 +1,7 @@
 # config/what_happened.rb
 specify do
-  creating_message do
-    notifies { |message| message.recipient }
+  creating_group_membership do
+    notifies { |group_membership| group_membership.group.user }
+    notifies { |group_membership| group_membership.group }
   end
 end
