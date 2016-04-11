881c6a9fc62ef52942f60e02ffccec9f9e9314dc
Tawan Sierek <tawan.sierek@gmail.com>
2016-04-11 18:14:59 +0200

    [Demo app] Fix typo

--- spec/integration/rails_app/db/migrate/20160127152258_add_some_groups.rb
+++ spec/integration/rails_app/db/migrate/20160127152258_add_some_groups.rb
@@ -1,7 +1,7 @@
 class AddSomeGroups < ActiveRecord::Migration
   def change
     WhatHappened.config.disabled do
-      [ "Ruby Meetup", "Snowboad Enthusiasts", "Railscamp", "Chess Club" ].each do |n|
+      [ "Ruby Meetup", "Snowboard Enthusiasts", "Railscamp", "Chess Club" ].each do |n|
         Group.create(name: n)
       end
     end
