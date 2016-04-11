42bb8006b5cc964ff1275d1ac4d1ee8a24028546
Tawan Sierek <tawan.sierek@gmail.com>
2016-04-11 18:07:53 +0200

    [Demo app] Split notifications for groups

--- spec/integration/rails_app/config/notification_routing.rb
+++ spec/integration/rails_app/config/notification_routing.rb
@@ -2,14 +2,17 @@
 creating_membership do
   sends_notification :group_has_new_member do
     to { |membership| membership.group }
+  end
+
+  sends_notification :one_of_your_groups_has_new_member do
     to { |membership| membership.group.members }
+    except_if { |recipient, membership| recipient == membership.user }
   end
 end
 
 creating_meeting do
   sends_notification :new_meeting_announced do
     to { |meeting| meeting.group.members }
-
     except_if { |recipient, meeting| recipient == meeting.creator }
   end
 end
