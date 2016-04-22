9485e0153464241bf69e9addcad92237573a6aff
Tawan Sierek <tawan.sierek@gmail.com>
2016-04-22 16:01:47 +0200

    Run integration and core tests in parallel

--- run_tests.rb
+++ run_tests.rb
@@ -1,11 +1,22 @@
 #!/usr/bin/env ruby
 
 current_dir = File.expand_path(File.dirname(__FILE__))
-system("docker run -v #{current_dir}:/usr/src/app tawan/what-happened:latest bundle exec rspec spec")
-exitstatus = $?.exitstatus > 0 ? $?.exitstatus : 0
-system("docker run -v #{current_dir}/integration_tests/rails_app:/usr/src/app -v #{current_dir}:/usr/src/gem tawan/what-happened:rails-4-app bundle exec rspec spec")
-if  exitstatus > 0
-  exit exitstatus
-else
+pids = [ ]
+pids << fork do
+  puts "running gem specs"
+  system("docker run -v #{current_dir}:/usr/src/app tawan/what-happened:latest bundle exec rspec spec")
   exit $?.exitstatus
 end
+pids << fork do
+  puts "running integration tests for Rails 4"
+  system("docker run -v #{current_dir}/integration_tests/rails_app:/usr/src/app -v #{current_dir}:/usr/src/gem tawan/what-happened:rails-4-app bundle exec rspec spec")
+  exit $?.exitstatus
+end
+
+exitstatus = 0
+pids.each do |pid|
+  Process.wait(pid)
+  exitstatus += $?.exitstatus
+end
+
+exit exitstatus
