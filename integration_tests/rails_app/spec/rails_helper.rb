50379167a63249312d5ab8134bb26ff7151417c7
Tawan Sierek <tawan.sierek@gmail.com>
2016-04-22 14:11:57 +0200

    Fix build #110

--- integration_tests/rails_app/spec/rails_helper.rb
+++ integration_tests/rails_app/spec/rails_helper.rb
@@ -26,7 +26,7 @@ require 'rake'
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
+#ActiveRecord::Migration.maintain_test_schema!
 
 require File.expand_path('./helpers.rb', File.dirname(__FILE__))
 require File.expand_path('./controller_helpers.rb', File.dirname(__FILE__))
